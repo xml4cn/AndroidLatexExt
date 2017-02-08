0ce2d8ebd45b2910098b6d93e9e6b7e8f0c67fa2
yangzc <yangzc@knowbox.cn>
2017-02-08 13:29:16 +0800

    init commit

--- app/src/main/java/com/hyena/latex/samples/MainActivity.java
+++ app/src/main/java/com/hyena/latex/samples/MainActivity.java
@@ -3,11 +3,16 @@ package com.hyena.latex.samples;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import maximsblog.blogspot.com.jlatexmath.core.AjLatexMath;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化
+        AjLatexMath.init(this);
+
     }
 }
